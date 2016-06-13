9b83bb9e8df318ca5543302c1b096451d5c1c9df
aa317016589 <317016589@qq.com>
2016-06-14 00:10:49 +0800

    first

---
+++ OnlineCheck/JudgeModes.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel;
+
+namespace OnlineCheck
+{
+    /// <summary>
+    /// 阅卷模式
+    /// </summary>
+    public enum JudgeModes
+    {
+        /// <summary>
+        /// 单评
+        /// </summary>
+        [Description("单评")]
+        SingleReview = 1,
+
+        /// <summary>
+        /// 双评
+        /// </summary>
+        [Description("双评")]
+        MultiReview,
+
+        /// <summary>
+        /// 2+1 模式
+        /// </summary>
+        [Description("2+1 模式")]
+        ThirdReview,
+
+        /// <summary>
+        /// 2+1+1 模式
+        /// </summary>
+        [Description("2+1+1 模式")]
+        FourReview
+    }
+
+    public enum CheckModes
+    {
+        CheckFirst = 1,
+        CheckSecond,
+        CheckThird,
+        CheckArbitration
+    }
+}
---
+++ OnlineCheck/QuestionGroup.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace OnlineCheck
+{
+    public class QuestionGroup
+    {
+        public Int32 QuestionGroupId { get; set; }
+
+
+        public JudgeModes JudgeMode { get; set; }
+
+        public Queue<Question> Questions { get; set; }
+
+
+    }
+
+    public class Question
+    {
+        public Int32 QuestionId { get; set; }
+
+        public Int32 StudentSubjectId { get; set; }
+
+        public String PicUrl { get; set; }
+
+        public JudgeModes JudgeMode { get; set; }
+
+        public Int32 FinalScore { get; set; }
+
+        public Int32 IsFinish { get; set; }
+
+        private List<TeacherCheck> _teacherChecks;
+
+        public void AddTeacherChecks(TeacherCheck teacherCheck)
+        {
+            Int32 len = _teacherChecks.Count;
+
+            switch (JudgeMode)
+            {
+                case JudgeModes.SingleReview:
+                    if (len > 0)
+                    {
+                        return;
+                    }
+                    break;
+                case JudgeModes.MultiReview:
+
+                    if (len > 1)
+                    {
+                        return;
+                    }
+
+                    break;
+                case JudgeModes.ThirdReview:
+
+
+                    break;
+                case JudgeModes.FourReview:
+                    break;
+            }
+
+            _teacherChecks.Add(teacherCheck);
+        }
+
+        public Boolean HaveDoubt()
+        {
+            return _teacherChecks.Any(s => s.IsDoubt);
+        }
+
+
+        public void Statistics()
+        {
+            //最后算分数
+        }
+    }
+
+    public class TeacherCheck
+    {
+        public Int32 TeacherId { get; set; }
+
+        public Int32 Score { get; set; }
+
+        public Boolean IsDoubt { get; set; }
+
+        public CheckModes CheckMode { get; set; }
+    }
+
+
+    public abstract class Checkx
+    {
+        
+    }
+}
